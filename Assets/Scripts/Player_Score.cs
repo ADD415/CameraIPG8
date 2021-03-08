237b2431aa03b845d52e023f30c3a5a27018adb5
ADD415 <ADAVID212@caledonian.ac.uk>
2021-03-08 13:38:30 +0000

    Camera added

---
+++ Assets/Scripts/Player_Score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Score : MonoBehaviour
+{
+    public int playerScore = 0;
+    public GameObject playerScoreUI;
+
+    void Update()
+    {
+        playerScoreUI.gameObject.GetComponent<Text>().text = ("Score: " + playerScore);
+    }
+
+    void OnTriggerEnter2D (Collider2D trig)
+    {
+
+        if (trig.gameObject.name == "Object")
+        {
+            playerScore += 10;
+            Destroy(trig.gameObject);
+        }
+    }
+}  
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text life;
+    int lives = 3;
+    private float time = 0;
+    public GameObject timeUI;
+
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        timeUI.gameObject.GetComponent<Text>().text = ("Time: " + (int)time);
+    }
+
+    public void lifeLost()
+    {
+        lives--;
+        life.text = lives.ToString();
+    }
+
+}
+
+
